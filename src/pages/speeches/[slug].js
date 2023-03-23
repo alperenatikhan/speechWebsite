4ae79c0a89e47f79c38378dd498d3f906b9027fe
alp <alperenmyung@gmail.com>
2023-03-23 21:22:04 +0100

    static pages generated

--- src/pages/speeches/[slug].js
+++ src/pages/speeches/[slug].js
@@ -1,34 +1,33 @@
-import { useRouter } from 'next/router';
-import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
 
-
-export default function Speeches({data}){
-
-  const router = useRouter();
-  const { slug } = router.query;
-
-  if (!data) {
-    return <p>Loading...</p>;
-  }
-
+function Speech({ page }) {
   return (
-    <>
-      {data.pageData?.map(item=> <div style={{width: '90%', minWidth:'250px',maxWidth:"600px" ,margin:'100px auto', padding:'2em', border:'1px solid lightgray'  }}><pre style={{display:'block',whiteSpace: "pre", fontFamily:'sans-serif', lineHeight: '1.5em', overflowX:"auto", whiteSpace: "pre-wrap",
-  wordWrap: "break-word", textAlign:'justify' }}> {item.pagecontent} </pre> </div> )}
-    </>
+    <div style={{width: '90%', minWidth:'250px',maxWidth:"600px" ,margin:'100px auto', padding:'2em', border:'1px solid lightgray'  }}> <pre style={{display:'block',whiteSpace: "pre", fontFamily:'sans-serif', lineHeight: '1.5em', overflowX:"auto", whiteSpace: "pre-wrap",
+    wordWrap: "break-word", textAlign:'justify' }}>{page.pagecontent}</pre> </div>
   );
 }
 
+export async function getStaticPaths() {
+  const filePath = path.join(process.cwd(), '/', 'correctedDataBase14.json');
+  const rawData = fs.readFileSync(filePath);
+  const pageData = JSON.parse(rawData);
 
+  const paths = pageData.map((data) => ({
+    params: { slug: data.slug },
+  }));
 
-
-export async function getServerSideProps({params}) {
- 
-  const { slug } = params
-  const data = await axios.get(`https://speech-website.vercel.app/speeches/${slug}`).then(data=> data.data)
-  
-
-  // Pass data to the page via props
-  return {props: { data }} 
-
+  return { paths, fallback: false };
 }
+
+export async function getStaticProps({ params }) {
+  const filePath = path.join(process.cwd(), '/', 'correctedDataBase14.json');
+  const rawData = fs.readFileSync(filePath);
+  const pageData = JSON.parse(rawData);
+
+  const page = pageData.find((data) => data.slug === params.slug);
+
+  return { props: { page } };
+}
+
+export default Speech;
