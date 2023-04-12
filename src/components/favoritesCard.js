d3f8db349685cf218c363b087ba17ac7e386784a
alp <alperenmyung@gmail.com>
2023-04-12 17:14:45 +0200

    add comment feature added

--- src/components/favoritesCard.js
+++ src/components/favoritesCard.js
@@ -19,13 +19,44 @@ import styles from '@/styles/Home.module.css'
 import indexStyles from '@/styles/index.module.css'
 import Link from 'next/link'
 import {useState} from 'react'
+import axios from 'axios'
 const inter = Inter({ subsets: ['latin'] })
+
 
 
 
 export default function FavoritesCard ({data}) {
 
+
   let [displayCommentBox ,setDisplayCommentBox] =  useState(false)
+  let [chosenCommentId, setChosenCommentId] = useState('')
+
+  const handleAddComment = async (event) => {
+    
+    event.preventDefault();
+  
+    const commentTextToSend = event.target.commentText.value;
+  
+    const commentData = {
+      commentId: chosenCommentId,
+      newCommentText: commentTextToSend,
+    };
+  
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(commentData),
+    };
+  
+    console.log(options)
+await fetch('/api/updatecomment', options).then(res=> {console.log(res); setDisplayCommentBox(false)}).catch(err=> console.log(err))
+
+  };
+
+
+
 
 return ( 
 
@@ -40,9 +71,26 @@ return (
 <h3 className={inter.className}> {data.searchKeyword} </h3> 
 
 
-{data?.comments.map(comment => 
-    <div className={indexStyles.textCard} style={{width:"95%", padding:'1.2em'}}> 
-
+{data.comments?.sort((a, b) => {
+  const aComment = a.comments?.reduce((acc, comment) => {
+    return comment.commentDate > acc.commentDate ? comment : acc;
+  }, a.comments[0]);
+  const bComment = b.comments?.reduce((acc, comment) => {
+    return comment.commentDate > acc.commentDate ? comment : acc;
+  }, b.comments[0]);
+  if (!aComment && !bComment) {
+    return 0;
+  } else if (!aComment) {
+    return 1;
+  } else if (!bComment) {
+    return -1;
+  } else {
+    return bComment.commentDate - aComment.commentDate;
+  }
+}).map((comment, index )=> 
+    <div key={index} className={indexStyles.textCard} style={{width:"95%", padding:'1.2em'}}> 
+    
+ 
 {comment.commentText.length > 0 ? 
 (<>
 
@@ -62,14 +110,17 @@ return (
 <button className= {indexStyles.textCardButton}  onClick={() => setDisplayCommentBox(!displayCommentBox)}> Add Comment </button>
 </div>
 {displayCommentBox &&
-(<form>
+(<form onSubmit = {(event) => handleAddComment(event)}>
 <textarea rows={5} cols={40} style={{boxSizing: "border-box", width: "100%",height: "200px", 
   padding: "10px",
   fontSize: "14px",
-  border: "1px solid #ccc"}}/>
+  border: "1px solid #ccc"}}
+  name= "commentText"
+  
+  />
 
 <div style={{margin:'0.5em', display: 'flex', justifyContent:'space-evenly'}}>
-<button className= {indexStyles.textCardButton}  type='submit'> Submit </button>
+<button className= {indexStyles.textCardButton} onClick={() => setChosenCommentId(comment._id)} type='submit'> Submit </button>
 <button style={{backgroundColor:'white', color:'dodgerblue'}} className= {indexStyles.textCardButton} onClick={()=> setDisplayCommentBox(false)}> Cancel</button>
 </div>
 </form>)
